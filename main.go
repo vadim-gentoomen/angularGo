3cddf12e41032755f11e1b976fb1ced1b48abb63
Vadim Dynnik <vadim.dynnik@gmail.com>
2019-07-02 12:28:15 +0300

    first commit

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"github.com/gin-gonic/contrib/static"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.Use(static.Serve("/", static.LocalFile("./ui/dist", true)))
+
+	router.Run(":8080")
+
+}
